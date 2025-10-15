a2655bc7e7c736258c975fc019147c4221f7e23c
Glauber Ferraz <102560669+glaubermferraz@users.noreply.github.com>
2025-10-15 18:53:02 -0300

    Add album rating feature and IAvaliavel interface
    Introduces the ability to rate albums via the new MenuAvaliarAlbum class and updates the menu options accordingly. Adds the IAvaliavel interface and implements it in Album and Banda, allowing both to handle ratings and calculate average scores.

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -17,7 +17,8 @@ Dictionary<int, Menu> opcoesDoMenu = new()
     { 2, new MenuRegistrarAlbum() },
     { 3, new MenuMostrarBandas() },
     { 4, new MenuAvaliarBanda() },
-    { 5, new MenuExibirDetalhes() },
+    { 5, new MenuAvaliarAlbum() },
+    { 6, new MenuExibirDetalhes() },
     {-1, new MenuSair() }
 };
 
@@ -42,7 +43,8 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite 2 para registrar o álbum de uma banda");
     Console.WriteLine("Digite 3 para mostrar todas as bandas");
     Console.WriteLine("Digite 4 para avaliar uma banda");
-    Console.WriteLine("Digite 5 para exibir os detalhes de uma banda");
+    Console.WriteLine("Digite 5 para avaliar um album");
+    Console.WriteLine("Digite 6 para exibir os detalhes de uma banda");
     Console.WriteLine("Digite -1 para sair");
 
     Console.Write("\nDigite a sua opção: ");
